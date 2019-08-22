4be4f9cf154df939a80131ef40ce341cccdc6a66
JessieW0010 <jessiewang@Jessies-MacBook-Pro.local>
2019-08-22 16:32:33 -0400

    added admin/sale index page which displays expired, upcoming, and active sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,15 @@
 class Sale < ActiveRecord::Base
+
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    start_on > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
+
 end
