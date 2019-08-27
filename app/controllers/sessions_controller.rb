68e268528b0960593a29e6d73b5ac3ef67cc2ac2
JessieW0010 <jessiewang@Jessies-MacBook-Pro.local>
2019-08-27 15:02:47 -0400

    fixed nav-bar user login status

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
 
   def create
 
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
-      session[:user_id] = user.id
+    if @user = User.authenticate_with_credentials(params[:email], params[:password])
+      session[:user_id] = @user.id
       redirect_to '/'
     else 
       redirect_to '/login'
